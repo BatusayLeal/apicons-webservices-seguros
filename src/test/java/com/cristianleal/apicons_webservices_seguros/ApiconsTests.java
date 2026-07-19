15a2e4135de762034fc66d8e7d16b6fd6ea44513
clealp <crislealcelu@gmail.com>
2026-07-19 10:48:58 -0400

    Conf Inicial Projecto APIcons a GitHub

---
+++ src/test/java/com/cristianleal/apicons_webservices_seguros/ApiconsTests.java
@@ -0,0 +1,13 @@
+package com.cristianleal.apicons_webservices_seguros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiconsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
