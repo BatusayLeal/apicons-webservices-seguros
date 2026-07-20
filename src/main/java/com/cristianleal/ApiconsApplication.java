601d5bcfee666c1ed3dc96ff6948daa5669b4247
clealp <crislealcelu@gmail.com>
2026-07-20 14:37:29 -0400

    Errores de carpeta donde estaba la clase Main

---
+++ src/main/java/com/cristianleal/ApiconsApplication.java
@@ -0,0 +1,12 @@
+package com.cristianleal.apicons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiconsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiconsApplication.class, args);
+    }
+}
